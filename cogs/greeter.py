5dec375cf738f2fd2a74b5d8c8ecf21e1c33e7cd
iLxlo <98545753+iLxlo@users.noreply.github.com>
2023-11-13 01:01:34 +0300

    Update greeter.py
    Fixed the str error

--- cogs/greeter.py
+++ cogs/greeter.py
@@ -7,6 +7,7 @@ import time
 import os
 from typing import Union
 from colorama import Fore
+import configparser
 
 data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
 database_path = os.path.join(data_folder, 'database.ini')
@@ -17,6 +18,10 @@ class Greeter(commands.Cog):
         self.client = client
         self.join_data = {}
         self.database_path = database_path
+
+        # Create an instance of ConfigParser
+        self.config = configparser.ConfigParser()
+        self.config.read(database_path)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -35,7 +40,7 @@ class Greeter(commands.Cog):
                 self.join_data[guild_id]['members'].append(member.mention)
 
                 if self.join_data[guild_id]['counter'] >= threshold:
-                    welcome_message = f"Welcome {' '.join(self.join_data[guild_id]['members'])}! checkout <#1130650031771373609> for free nitro!"
+                    welcome_message = f"Welcome {' '.join(self.join_data[guild_id]['members'])}!"
                     greeting_message = await welcome_channel.send(welcome_message)
 
                     self.join_data[guild_id]['counter'] = 0
@@ -78,9 +83,9 @@ class Greeter(commands.Cog):
                 async for message in welcome_channel.history(limit=1):
                     await message.delete(delay=delay_seconds)
 
-                self.database_path[f'GUILD:{guild_id}']['DELETER'] = str(delay_seconds)
+                self.config[f'GUILD:{guild_id}']['DELETER'] = str(delay_seconds)
                 with open(database_path, 'w') as configfile:
-                    self.database_path.write(configfile)
+                    self.config.write(configfile)
 
                 update_message = await ctx.send(f"Greeter message will be deleted after {delay_seconds} seconds.")
                 await asyncio.sleep(5)
@@ -89,20 +94,20 @@ class Greeter(commands.Cog):
     @greeter.command(name='stop')
     @has_permissions(manage_messages=True)
     async def stop(self, ctx):
-            guild_id = ctx.guild.id
-            self.set_guild_settings(guild_id, None, 1)  # Set channel_id to None
-            message = await ctx.send('Greeter functionality has been stopped.')
-            await asyncio.sleep(3)
-            await message.delete()
+        guild_id = ctx.guild.id
+        self.set_guild_settings(guild_id, None, 1)  # Set channel_id to None
+        message = await ctx.send('Greeter functionality has been stopped.')
+        await asyncio.sleep(3)
+        await message.delete()
 
     async def schedule_deletion(self, message, delay_seconds):
         await asyncio.sleep(delay_seconds)
         await message.delete()
 
     def get_guild_settings(self, guild_id):
-        if f'GUILD:{guild_id}' in self.database_path:
-            channel_id = self.database_path[f'GUILD:{guild_id}'].get('channel', fallback=None)
-            threshold = self.database_path[f'GUILD:{guild_id}'].getint('threshold', fallback=1)
+        if f'GUILD:{guild_id}' in self.config:
+            channel_id = self.config[f'GUILD:{guild_id}'].get('channel', fallback=None)
+            threshold = self.config[f'GUILD:{guild_id}'].getint('threshold', fallback=1)
             channel_id = int(channel_id) if channel_id and channel_id.lower() != 'none' else None
 
             return channel_id, threshold
@@ -110,9 +115,9 @@ class Greeter(commands.Cog):
         return None, None
 
     def set_guild_settings(self, guild_id, channel_id, threshold):
-        self.database_path[f'GUILD:{guild_id}'] = {'channel': str(channel_id), 'threshold': str(threshold)}
-        with open(self.database_path, 'w') as configfile:
-            self.database_path.write(configfile)
+        self.config[f'GUILD:{guild_id}'] = {'channel': str(channel_id), 'threshold': str(threshold)}
+        with open(database_path, 'w') as configfile:
+            self.config.write(configfile)
 
 
 async def setup(client):
